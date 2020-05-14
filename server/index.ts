4b0e89bbfa355d3a18c48cec49407b5a24a10f0f
d4 <15785841+1dotd4@users.noreply.github.com>
2020-05-14 16:24:39 +0200

    test prezzo

--- server/index.ts
+++ server/index.ts
@@ -167,6 +167,7 @@ app.post('/deploy', (req, res) => {
                   * 1.1;
                 let priceInWei = Math.floor(price * 0.01);
                 priceInWei += Number(funcDataObj.fee);
+                console.log(priceInWei);
                 smartHandler.terminateDeploy(funcName,
                   tokens[proof].devAddress,
                   priceInWei,
